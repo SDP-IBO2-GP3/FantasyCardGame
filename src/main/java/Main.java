6a595608293b3530da1cd7d7ee72f04195b2c331
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2017-10-13 20:43:32 +0200

    First Commit : Project Init 2 libraries added : log4j + junit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args){
+        System.out.println("Hello Wolrd ! This is our amazing card game  ");
+
+        LOGGER.error("Hello world it doesn't work for now .. ");
+    }
+}
