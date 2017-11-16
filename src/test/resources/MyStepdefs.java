876ebe4a412d2e5ad6c122ae01422dd590b76fab
felixlarrouy <felix.larrouy@gmail.com>
2017-11-16 18:27:19 +0100

    Test repo, cucumber library

---
+++ src/test/resources/MyStepdefs.java
@@ -0,0 +1,35 @@
+import com.sun.xml.internal.bind.v2.TODO;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+
+public class MyStepdefs {
+
+    @Then("^Player1 has (\\d+) cards in his hand$")
+    public void playerHasCardsInHisHand(int arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new PendingException();
+    }
+
+    @And("^Player1 has (\\d+) cards in his kingdom$")
+    public void playerHasCardsInHisKingdom(int arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new PendingException();
+    }
+
+    @Then("^Deck has (\\d+) cards$")
+    public void deckHasCards(int arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new PendingException();
+    }
+
+    @And("^Different races$")
+    public void differentRaces(int arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new PendingException();
+    }
+}
