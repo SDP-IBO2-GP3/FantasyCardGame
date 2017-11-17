7fb4cfffa663ef687d02eed4e94beb04e9c51977
eddinekdr <kouider.eddine@gmail.com>
2017-11-17 15:01:38 +0100

    Add of different races of cards. Add of description of races. Generate of Deck. Shuffle the Deck.

--- src/main/java/edu/insightr/fantasycardgame/BoardController.java
+++ src/main/java/edu/insightr/fantasycardgame/BoardController.java
@@ -1,4 +1,5 @@
 package edu.insightr.fantasycardgame;
+
 import java.util.List;
 
 /**
--- src/main/java/edu/insightr/fantasycardgame/Deck.java
+++ src/main/java/edu/insightr/fantasycardgame/Deck.java
@@ -1,15 +1,16 @@
 package edu.insightr.fantasycardgame;
 
+import java.util.Collections;
 import java.util.Stack;
 
 
 /**
  * <b>A deck present at the center of our board</b><br><br>
- *
+ * <p>
  * Attributes :
  * <ul>
- *      <li>listCard - The list of cards in the deck</li>
- *      <li>numberOfCards - Maximum number of cards in the deck</li>
+ * <li>listCard - The list of cards in the deck</li>
+ * <li>numberOfCards - Maximum number of cards in the deck</li>
  * </ul>
  */
 public class Deck {
@@ -27,16 +28,16 @@ public class Deck {
      * Default constructor for our deck.
      */
     public Deck() {
-        this.numberOfCards = 50;
+        this.numberOfCards = 60;
         this.listCard = new Stack<Card>();
     }
 
     /**
      * Getter for the size of the deck.
+     *
      * @return The deck's size
      */
-    public int getSize()
-    {
+    public int getSize() {
         return listCard.size();
     }
 
@@ -48,17 +49,31 @@ public class Deck {
      * <li>Also, each race should be represented at least once.</li>
      * </ul>
      */
-    public void fillDeck()
-    {
-        // TODO : Implement this method and write tests
+    public void fillDeck() {
+        for (int i = 0; i < numberOfCards/6; i++) {
+            for (Card.Race race : Card.Race.values()
+                    ) {
+                this.listCard.push(new Card(race));
+            }
+        }
     }
+    public void shuffleDeck(){
+        Collections.shuffle(this.listCard);
+    }
+    /**
+     *
+     * @return
+     */
+    public String toString() {
+            return "voici les cartes \n" + listCard;
+        }
 
     /**
      * Gets the top card in the deck and discards it from it.
+     *
      * @return An instance of card
      */
-    public Card getACard()
-    {
+    public Card getACard() {
         return this.listCard.pop();
     }
 }
