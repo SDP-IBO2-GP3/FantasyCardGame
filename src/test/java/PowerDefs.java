d273e31729ea9b185254a8415114c18cdf015ffc
pousspouss <hugo.pousseur@gmail.com>
2017-11-23 09:56:28 +0100

    Creation before for the power.feature file

--- src/test/java/PowerDefs.java
+++ src/test/java/PowerDefs.java
@@ -1,3 +1,4 @@
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -6,27 +7,64 @@ import edu.insightr.fantasycardgame.BoardController;
 import edu.insightr.fantasycardgame.Card;
 import edu.insightr.fantasycardgame.Player;
 
+import java.util.Random;
+
 public class PowerDefs {
+
+
+    public void createCardKingdom(Player player,int sizeRandom){
+
+        Random rand = new Random();
+
+        // Assure that all kind race is distributed
+        for(Card.Race race : Card.Race.values()){
+            player.addACardKingdom(new Card(race));
+        }
+
+        // Add randomly cards on the deck
+        // sizeRandom allows to change the size if each deck
+        for(int i=0;i<sizeRandom;i++){
+            int value = rand.nextInt(6);
+            Card cardAdd = new Card(Card.Race.values()[value]);
+            player.addACardKingdom(cardAdd);
+        }
+
+
+
+    }
+
+
+    @Before
+    public void setUp(){
+
+
+        player2 = new Player(true);
+        player1 = new Player(false);
+
+        board = new BoardController();
+        board.initiliazeGame();
+
+        createCardKingdom(player1,10);
+        createCardKingdom(player2,5);
+
+
+
+    }
 
     private Player player1, player2;
     private BoardController board;
+
+    private int sizePlayer1BeforeEffect;
+    private int sizePlayer2BeforeEffect;
+    private int scorePlayer1BeforeEffect;
+    private int scorePlayer2BeforeEffect;
 
     /**
      * Scenario: Troll Card Power
      */
     @Given("^The Player1 and Player2 cards$")
     public void thePlayerAndPlayerCards() throws Throwable {
-        player2 = new Player(true);
-        player1 = new Player(false);
 
-        Card korrigan = new Card(Card.Race.Korrigan);
-        Card dryad = new Card(Card.Race.Dryad);
-        Card gnome = new Card(Card.Race.Gnome);
-
-
-        player1.addACardKingdom(dryad);
-        player2.addACardKingdom(korrigan);
-        player2.addACardKingdom(gnome);
     }
 
     @When("^Player1 is playing the Troll$")
@@ -36,19 +74,28 @@ public class PowerDefs {
         player1.addACard(troll);
         board = new BoardController();
         board.initiliazeGame();
+
+        sizePlayer1BeforeEffect = player1.getListCardsKingdom().size();
+        sizePlayer2BeforeEffect = player2.getListCardsKingdom().size();
+
+        scorePlayer1BeforeEffect = player1.getScore();
+        scorePlayer2BeforeEffect = player2.getScore();
+
         board.applyEffect(player1, player2, troll);
     }
 
     @Then("^The players swap kingdoms$")
     public void thePlayersSwapKingdoms() throws Throwable {
-        org.junit.Assert.assertEquals(2, player1.getListCardsKingdom().size());
-        org.junit.Assert.assertEquals(1, player2.getListCardsKingdom().size());
+        org.junit.Assert.assertEquals(sizePlayer2BeforeEffect, player1.getListCardsKingdom().size());
+        org.junit.Assert.assertEquals(sizePlayer1BeforeEffect, player2.getListCardsKingdom().size());
         org.junit.Assert.assertEquals(Card.Race.Korrigan, player1.getListCardsKingdom().get(0).getRace());
     }
 
     @And("^The players swap scores$")
     public void thePlayersSwapScores() throws Throwable {
-        org.junit.Assert.assertEquals(2, player1.getScore());
-        org.junit.Assert.assertEquals(1, player2.getScore());
+
+
+        org.junit.Assert.assertEquals(scorePlayer2BeforeEffect, player1.getScore());
+        org.junit.Assert.assertEquals(scorePlayer1BeforeEffect, player2.getScore());
     }
 }
