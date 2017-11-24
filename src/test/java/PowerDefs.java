309d6bfbaf1a463b3919f3343a2316bc2edba0a2
felixlarrouy <felix.larrouy@gmail.com>
2017-11-24 16:49:44 +0100

    Added power tests

--- src/test/java/PowerDefs.java
+++ src/test/java/PowerDefs.java
@@ -1,3 +1,4 @@
+import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -6,7 +7,10 @@ import cucumber.api.java.en.When;
 import edu.insightr.fantasycardgame.BoardController;
 import edu.insightr.fantasycardgame.Card;
 import edu.insightr.fantasycardgame.Player;
+import org.junit.Assert;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PowerDefs {
@@ -37,65 +41,100 @@ public class PowerDefs {
     @Before
     public void setUp(){
 
-
-        player2 = new Player(true);
-        player1 = new Player(false);
-
         board = new BoardController();
         board.initiliazeGame();
 
-        createCardKingdom(player1,10);
-        createCardKingdom(player2,5);
+        createCardKingdom(board.getPlayer1(),10);
+        createCardKingdom(board.getPlayer2(),5);
 
 
 
     }
 
-    private Player player1, player2;
     private BoardController board;
 
     private int sizePlayer1BeforeEffect;
     private int sizePlayer2BeforeEffect;
     private int scorePlayer1BeforeEffect;
     private int scorePlayer2BeforeEffect;
+    private ArrayList<Card> handPlayer1BeforeEffect;
+    private ArrayList<Card> handPlayer2BeforeEffect;
 
     /**
      * Scenario: Troll Card Power
      */
-    @Given("^The Player1 and Player2 cards$")
-    public void thePlayerAndPlayerCards() throws Throwable {
-
-    }
 
     @When("^Player1 is playing the Troll$")
     public void playerIsPlayingTheTroll() throws Throwable {
         Card troll = new Card(Card.Race.Troll);
-        //player1.addACardKingdom(player1.getListCardsInHand().get(0));
-        player1.addACard(troll);
-        board = new BoardController();
-        board.initiliazeGame();
+        board.getPlayer1().addACardKingdom(troll);
 
-        sizePlayer1BeforeEffect = player1.getListCardsKingdom().size();
-        sizePlayer2BeforeEffect = player2.getListCardsKingdom().size();
+        sizePlayer1BeforeEffect = board.getPlayer1().getListCardsKingdom().size();
+        sizePlayer2BeforeEffect = board.getPlayer2().getListCardsKingdom().size();
 
-        scorePlayer1BeforeEffect = player1.getScore();
-        scorePlayer2BeforeEffect = player2.getScore();
+        scorePlayer1BeforeEffect = board.getPlayer1().getScore();
+        scorePlayer2BeforeEffect = board.getPlayer2().getScore();
 
-        board.applyEffect(player1, player2, troll);
+        board.applyEffect(board.getPlayer1(), board.getPlayer2(), troll);
     }
 
     @Then("^The players swap kingdoms$")
     public void thePlayersSwapKingdoms() throws Throwable {
-        org.junit.Assert.assertEquals(sizePlayer2BeforeEffect, player1.getListCardsKingdom().size());
-        org.junit.Assert.assertEquals(sizePlayer1BeforeEffect, player2.getListCardsKingdom().size());
-        org.junit.Assert.assertEquals(Card.Race.Korrigan, player1.getListCardsKingdom().get(0).getRace());
+        org.junit.Assert.assertEquals(sizePlayer2BeforeEffect, board.getPlayer1().getListCardsKingdom().size());
+        org.junit.Assert.assertEquals(sizePlayer1BeforeEffect, board.getPlayer2().getListCardsKingdom().size());
+        org.junit.Assert.assertEquals(Card.Race.Korrigan, board.getPlayer1().getListCardsKingdom().get(0).getRace());
     }
 
     @And("^The players swap scores$")
     public void thePlayersSwapScores() throws Throwable {
+        org.junit.Assert.assertEquals(scorePlayer2BeforeEffect, board.getPlayer1().getScore());
+        org.junit.Assert.assertEquals(scorePlayer1BeforeEffect, board.getPlayer2().getScore());
+    }
 
+    /**
+     * Scenario: Goblin Card Power
+     */
 
-        org.junit.Assert.assertEquals(scorePlayer2BeforeEffect, player1.getScore());
-        org.junit.Assert.assertEquals(scorePlayer1BeforeEffect, player2.getScore());
+    @When("^Player1 is playing a Goblin$")
+    public void playerIsPlayingAGoblin() throws Throwable {
+        Card goblin = new Card(Card.Race.Goblin);
+        board.getPlayer1().addACardKingdom(goblin);
+
+        handPlayer1BeforeEffect = board.getPlayer1().getListCardsInHand();
+        handPlayer2BeforeEffect = board.getPlayer2().getListCardsInHand();
+
+        board.applyEffect(board.getPlayer1(), board.getPlayer2(), goblin);
+    }
+
+    @Then("^The players swap hands$")
+    public void thePlayersSwapHands() throws Throwable {
+        Assert.assertEquals(handPlayer1BeforeEffect, board.getPlayer2().getListCardsInHand());
+        Assert.assertEquals(handPlayer2BeforeEffect, board.getPlayer1().getListCardsInHand());
+    }
+
+    /**
+     * Scenario: Korrigan Card Power
+     */
+
+    @When("^Player1 is playing a Korrigan$")
+    public void playerIsPlayingAKorrigan() throws Throwable {
+        Card korrigan = new Card(Card.Race.Korrigan);
+        board.getPlayer1().addACardKingdom(korrigan);
+
+        handPlayer1BeforeEffect = board.getPlayer1().getListCardsInHand();
+        handPlayer2BeforeEffect = board.getPlayer2().getListCardsInHand();
+        System.out.println(handPlayer1BeforeEffect.size());
+        System.out.println(handPlayer2BeforeEffect.size());
+        board.applyEffect(board.getPlayer1(), board.getPlayer2(), korrigan);
+    }
+
+    @Then("^Player2 has two cards less in his hand$")
+    public void playerHasTwoCardsLessInHisHand() throws Throwable {
+        Assert.assertEquals(handPlayer2BeforeEffect.size()-2, board.getPlayer2().getListCardsInHand().size());
+    }
+
+    @And("^Player1 has two more cards in his hand$")
+    public void playerHasTwoMoreCardsInHisHand() throws Throwable {
+        Assert.assertEquals(handPlayer1BeforeEffect.size()+2, board.getPlayer1().getListCardsInHand().size());
     }
 }
--- src/test/resources/power.feature
+++ src/test/resources/power.feature
@@ -2,9 +2,17 @@
 # Tags: optional
     
 Feature: Each card Power
-    
+
 Scenario: Troll Card Power
-    Given The Player1 and Player2 cards
     When Player1 is playing the Troll
     Then The players swap kingdoms
     And The players swap scores
+
+Scenario: Goblin Card Power
+    When Player1 is playing a Goblin
+    Then The players swap hands
+
+Scenario: Korrigan Card Power
+    When Player1 is playing a Korrigan
+    Then Player2 has two cards less in his hand
+    And Player1 has two more cards in his hand
