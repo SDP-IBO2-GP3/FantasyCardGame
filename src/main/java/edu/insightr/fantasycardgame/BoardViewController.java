0cd4be7c3da3622ee4b3e52c544db3c10d68597f
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2017-11-24 17:55:01 +0100

    Took off useless method

--- src/main/java/edu/insightr/fantasycardgame/BoardViewController.java
+++ src/main/java/edu/insightr/fantasycardgame/BoardViewController.java
@@ -19,31 +19,4 @@ public class BoardViewController implements Initializable{
         // Reflechir sur quand on clique sur le deck, une carte sort aléatoirement
     }
 
-    public void getACard()
-    {
-        //System.out.println("on clique");
-        int cardId = (int) (Math.random() * 6 );
-        System.out.println(cardId);
-        Card card;
-        switch(cardId){
-            case 0:
-                card = new Card(Card.Race.Korrigan);
-                break;
-            case 1:
-                card = new Card(Card.Race.Troll);
-                break;
-            case 2:
-                card = new Card(Card.Race.Goblin);
-                break;
-            case 3:
-                card = new Card(Card.Race.Elf);
-                break;
-            case 4:
-                card = new Card(Card.Race.Dryad);
-                break;
-            case 5:
-                card = new Card(Card.Race.Gnome);
-                break;
-        }
-    }
 }
